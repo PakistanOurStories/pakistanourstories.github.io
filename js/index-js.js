af65c83db86ab51408dc104017d3e7993e4aa9a3
Saqib Ali <pysaqib@gmail.com>
2019-07-27 22:39:27 +0500

    v2.0
    Added a new homepage

---
+++ js/index-js.js
@@ -0,0 +1,15 @@
+// Article Information
+
+var path = "../articles/article-assets/cover-images/";
+
+var titles = [
+            ["Gurdwara Chowa Sahib, present at Rohtas Fort soon to be re-opened", path + "08.png"],
+            ["Another Pakistan Army’s major selected as an Instructor in Royal Military Academy, Sandhurst", path + "01.png"],
+            ["Pakistani authorities safely rescue Arizona based rock-climber Michelle Marco after she fell off from a Peak at Hispar Glacier", path + "02.jpg"],
+            ["An old man who physically arrested a student is now behind bars", path + "03.png"],
+            ["Kulbushan Yadav’s verdict by International Court of Justice: Which side won what", path + "04.jpg"],
+            ["An Iranian drone was captured by Pakistan which violated the air-space. Here’s what we know so far", path + "05.jpg"],
+            ["How Pakistanis reacted to Imran-Trump meeting might blow you away!", path + "06.jpg"],
+            ["From the people of Pakistan, a 100 bed hospital was inaugurated in Afghanistan", path + "07.png"]
+        ];
+
